0bbfba388c28bb60eee27dadf50e066d5c465bc3
Tim Becker <tim.becker@kuriositaet.de>
2012-11-15 08:35:08 +0100

    more tests

--- test_gcode.js
+++ test_gcode.js
@@ -1,6 +1,7 @@
 
 
-var test_dirs = ["test/cuttingedge", "test/tormach"]
+var  test_dirs = ["test/cuttingedge", "test/tormach"]
+    ,json_dir  = "test/json"
 var fs        = require("fs"),
     gcode     = require("./parser.js")
 var p         = console.log
@@ -18,7 +19,6 @@ function perr(data){
 
 function test(fn) {
   if (fn.match(/\.(cnc|tap)$/)) {
-  p(fn) 
     fs.readFile(fn, function(err, data) {
       if (err) {
         error(err)
@@ -31,6 +31,48 @@ function test(fn) {
   }
 }
 
+
+function compare(fn, should, is) {
+  if (should.length !== is.length) {
+    error("unequal lengths: "+fn+" should:"+should.length+" is:"+is.length)
+  }
+
+  for (var i = 0; i!=should.length; ++i) {
+    var  shld = should[i]
+        ,iz   = is[i]
+
+    for (var p in shld) {
+      if (shld[p] !== iz[p]) {
+        error("values unequal in event: "+i+" should:"+JSON.stringify(shld)+" is: "+JSON.stringify(iz))
+      }
+    }
+  }
+}
+
+function test_json(fn) {
+  
+  if (fn.match(/\.(cnc|tap)$/)) {
+
+    var fnjson = json_dir + "/" + fn.replace(/^test\//, '')+".json"
+    var json = JSON.parse(fs.readFileSync(fnjson))
+  
+    fs.readFile(fn, function(err, data) {
+      if (err) {
+        error(err)
+      }
+      var arr = []
+      function collect(ev) {
+        arr.push(ev)
+      }
+      var gcp = new gcode.GCode(collect)
+      if (!gcp.parse(data)) {
+        error("error parsing: "+fn)
+      }
+      compare(fn, json, arr)
+    })
+  }
+}
+
 test_dirs.forEach(function(dir){
   fs.readdir(dir, function(err,files) {
     if (err) {
@@ -38,5 +80,6 @@ test_dirs.forEach(function(dir){
     }
     files = files.map(function(f) {return dir+"/"+f})
     files.forEach(test)
+    files.forEach(test_json)
   })
 })
